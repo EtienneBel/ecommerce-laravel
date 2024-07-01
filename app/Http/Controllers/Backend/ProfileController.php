4596822b4c7c690a14e6cec25147b9e976d920aa
Wend-Kuuni <belemgnegreetienne@gmail.com>
2024-07-01 19:50:15 +0000

    added update profile

---
+++ app/Http/Controllers/Backend/ProfileController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+    function index()
+    {
+        return view('admin.profile.index');
+    }
+
+    function update(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'max:100'],
+            'email' => ['required', 'email', 'unique:users,email,' . Auth::user()->id]
+        ]);
+
+        $user = Auth::user();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
+
+        return redirect()->back();
+    }
+}
